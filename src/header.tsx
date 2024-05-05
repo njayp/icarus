7ec21d62dda19c4668900931e18e424765bb3d8b
nick powell <nickjaypowell@gmail.com>
2024-05-05 06:50:40 -0700

    pad header

--- src/header.tsx
+++ src/header.tsx
@@ -1,15 +1,15 @@
-import { Button, ButtonGroup, Stack } from "@mui/material";
+import { Button, ButtonBase, ButtonGroup, Stack } from "@mui/material";
 import blogTitle from './assets/title.svg'
 import { Link } from "react-router-dom";
 
 export function Header() {
-    return <Stack direction="row" justifyContent="space-between" alignItems="center">
+    return <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ marginTop: "1em" }}>
         <Link to="/" >
-            <Button>
+            <ButtonBase sx={{ marginLeft: "4em" }}>
                 <img src={blogTitle} className="logo" />
-            </Button>
+            </ButtonBase>
         </Link>
-        <ButtonGroup variant="text" aria-label="Basic button group">
+        <ButtonGroup variant="text" aria-label="Basic button group" sx={{ marginRight: "1em" }}>
             <Link to="/">
                 <Button>Home</Button>
             </Link>
